62852142ab641d7b6a7d05a8ab577cd5fe9031ac
Plags <thomasjouaux8@gmail.com>
2019-10-29 14:11:14 +0100

    p3ex1

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+let image=document.getElementById('image1');
+
+
+
+ image.onmouseenter = function() {
+   image.style.border='3px solid red' ;
+ };
+ image.onmouseout = function(){
+   image.style.border='none';
+ }
